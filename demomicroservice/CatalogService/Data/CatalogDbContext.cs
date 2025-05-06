31e2ade57f458f9077a34a4174a921853de2528f
SW115406 <SW115406@TALEW42NLPC2038>
2025-05-06 12:01:53 +0530

    created and tested catalog service project

---
+++ demomicroservice/CatalogService/Data/CatalogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CatelogService.Data;
+using CatalogService.Models;
+
+
+namespace CatelogService.Data
+{
+    public class CatelogDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public CatelogDbContext(DbContextOptions<CatelogDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ demomicroservice/CatalogService/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CatalogService.Models
+{
+    [Table("Products")]
+
+    public class Product
+    {
+        [Key]
+        [ScaffoldColumn(true)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(58)] 
+        public string? Name { get; set; }
+
+        [MaxLength(200)] public string? Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+
+        public decimal Price { get; set; }
+    }
+}
