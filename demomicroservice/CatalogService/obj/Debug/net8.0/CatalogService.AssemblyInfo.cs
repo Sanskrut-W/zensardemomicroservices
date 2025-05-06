8e4a218d26a42f7766e90e1d6b554f1b95518f7d
SW115406 <SW115406@TALEW42NLPC2038>
2025-05-06 15:34:42 +0530

    added more configuration for docker in Catalog Service project

--- demomicroservice/CatalogService/obj/Debug/net8.0/CatalogService.AssemblyInfo.cs
+++ demomicroservice/CatalogService/obj/Debug/net8.0/CatalogService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb202b5aa117a248d76760130ef66d31ef48520d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba18fe44663830febac2bb0f2967715d5244a90f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
